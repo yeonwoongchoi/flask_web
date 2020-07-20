50b1b836207150cba14c31aa3caa362863f3203e
unknown <yeonwoongchoi>
2020-07-20 16:08:22 +0900

    navbar

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 app.debug=True
-@app.route('/data')
+@app.route('/')
 def index():
     print("Success")
     # return "TEST"
-    return render_template('home.html')
+    return render_template('home.html', hello = "GaryKim")
+
+@app.route('/about')
+def about():
+    print("Success")
+    # return "TEST"
+    return render_template('about.html', hello = "GaryKim")
+
+@app.route('/articles')
+def articles():
+    print("Success")
+    # return "TEST"
+    return render_template('articles.html', hello = "GaryKim")
 
 if __name__ =='__main__':
     # app.run(host = '0.0.0.0', port='8080')
